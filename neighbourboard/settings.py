50a31bb4d8f77367a69ff0a6ba88c741a6f021ac
divinamary89 <divinamary89gmail.com>
2026-03-28 03:39:30 +0530

    added postgresql support

--- neighbourboard/settings.py
+++ neighbourboard/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'neighbourboard.wsgi.application'
 
 import mongoengine
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600
+    )
 }
 
 mongoengine.connect(
